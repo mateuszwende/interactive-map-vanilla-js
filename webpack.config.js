363b6aececa01d4bb03d0486ae7457503c5e0173
Mateusz Wende <matewen129@student.polsl.pl>
2020-05-21 14:57:52 +0200

    Add babel

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,17 @@ module.exports = (env, argv) => {
     module: {
       rules: [
         {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: ["@babel/plugin-proposal-object-rest-spread"],
+            },
+          },
+        },
+        {
           test: /\.s[ac]ss$/i,
           use: [
             "style-loader",
